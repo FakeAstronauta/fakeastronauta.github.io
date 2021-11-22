c5cd173b482a8bb2015c9661afa63b02acdf4fc6
Norman <pescado@crudo.com>
2021-11-21 21:38:34 -0600

    most changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+
 import  './NavBar.css'
 import  '../App.css'
 
-async function insertImage(){
-    let raw = await fetch('https://api.pexels.com/v1/photos/1126993',{ headers: {
-        Authorization: '563492ad6f917000010000017b6f3158f1794ee085b3def899f919dd'
-      }});
-    let data = await raw.json();
-    
-    // return cooked;
-    // console.log(cooked)
-}
-
-insertImage();
-
 export default function NavBar(){
+//     const [background, setBackground] = useState(null);
 
+//     useEffect(async ()=>{
+//         let raw = await fetch('https://api.pexels.com/v1/photos/1126993',{ headers: {
+//             Authorization: '563492ad6f917000010000017b6f3158f1794ee085b3def899f919dd'
+//         }});
+//         let data = await raw.json();
+//         setBackground(data)
+//     }, []);
     return(
         <nav className="nav-bar">
-            <div  className="first-container">
-
-                <Link to = '/' className='logo no-link-style'>MONTREAL BOTÉZ</Link>
-                <div className="icons">
-                    <Link to = '/' class="material-icons no-link-style">
-                        search
-                    </Link>
-                    <Link to = '/' class="material-icons no-link-style">
-                        person_outline
-                    </Link>
+            <div className="global-container" >
+                <div  className="first-container">
+                    <Link to = '/' className='logo-white no-link-style'>MONTREAL BOTÉZ</Link>
+                    <div className="nav-bar-icons">
+                        <Link to = '/' class="logo-white nav-bar-icon material-icons no-link-style">
+                            search
+                        </Link>
+                        <Link to = '/' class="logo-white material-icons no-link-style">
+                            person_outline
+                        </Link>
+                    </div>
+                    {/* {background && <img src={`${background.src.landscape})`}></img>} */}
                 </div>
-            <h1>{}</h1>
+                <div  className="second-container">
+                    <ul>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' >TODAY'S</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' >HOT OUTFITS</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' >OUR ACCESORIES</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' >FRAGANCES</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            {/* <img alt='No image found' src={insertImage().src.landscape}></img> */}
         </nav>
         );
 
